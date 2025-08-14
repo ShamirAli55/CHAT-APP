fa1b3ab07ff2f4581f24445b605c8fb7a16f16e6
ShamirAli55 <shamirali9779@gmail.com>
2025-08-14 00:00:22 -0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("API is running");
+app.get("/api/auth/signup",(req,res)=>{
+    res.send("Signup route");
 })
 
+app.get("/api/auth/signup",(req,res)=>{
+    res.send("Login route");
+})
+app.get("/api/auth/logout",(req,res)=>{
+    res.send("logout route");
+})
 app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 })
