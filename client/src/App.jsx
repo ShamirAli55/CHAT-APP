34348de594deaf67e5d4c763c98598536c0d38e5
ShamirAli55 <shamirali9779@gmail.com>
2025-09-03 12:43:24 +0500

    Routes Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {Routes as Router,Route} from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 const App = () => {
   return (
-    <div className='text-9xl'>App</div>
+    <div className="bg-[url('./assets/bgImage.svg')]">
+    <Router>
+      <Route index path="/" element={<HomePage/>}/>
+      <Route index path="/login" element={<LoginPage/>}/>
+      <Route index path="/profile" element={<ProfilePage/>}/>
+    </Router>
+    </div>
   )
 }
 
